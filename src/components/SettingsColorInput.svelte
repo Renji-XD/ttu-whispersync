f713f5fe5bc2f9cd2a78b26d4222741c7a4c4b9c
Renji-xD <trashmefull@hotmail.com>
2024-08-24 12:44:35 +0200

    feat: add ability to set color by hex code

--- src/components/SettingsColorInput.svelte
+++ src/components/SettingsColorInput.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Icon from './Icon.svelte';
+	import { skipKeys } from '../lib/actions';
+	import type { EventWithElement } from '../lib/general';
 	import { getDefaultSetting } from '../lib/settings';
-	import type { SettingsStore } from '../lib/stores';
+	import { skipKeyListener$, type SettingsStore } from '../lib/stores';
 	import { mdiRepeatVariant } from '@mdi/js';
 	import { createEventDispatcher } from 'svelte';
 
@@ -10,12 +12,32 @@
 
 	const id = targetStore$.key();
 	const dispatch = createEventDispatcher<{
+		change: void;
 		reset: void;
 	}>();
+
+	function onTextColorBlur({ currentTarget }: EventWithElement<HTMLInputElement>) {
+		if (!!currentTarget.value.match(/^#[0-9A-F]{6}$/i)) {
+			$targetStore$ = currentTarget.value;
+			dispatch('change');
+		} else {
+			currentTarget.value = $targetStore$;
+		}
+	}
 </script>
 
 <label for={id}>{label}</label>
-<input type="color" {id} bind:value={$targetStore$} on:change />
+<div class="flex items-center">
+	<input
+		type="text"
+		style="max-width: 50%;"
+		value={$targetStore$}
+		on:blur={onTextColorBlur}
+		use:skipKeys={{ document, isSkipped: $skipKeyListener$ }}
+	/>
+	<input class="flex-1 m-l-s" type="color" {id} bind:value={$targetStore$} on:change />
+</div>
+
 <button
 	title="Reset Color"
 	on:click={() => {
