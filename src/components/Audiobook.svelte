9d60218885d2ce7d7ed17f3f386539d68fafb5de
Renji-xD <trashmefull@hotmail.com>
2024-09-12 22:43:12 +0200

    feat: add ability to set audio file via cover click

--- src/components/Audiobook.svelte
+++ src/components/Audiobook.svelte
@@ -859,31 +859,33 @@
 </script>
 
 <div class="flex m-y-s">
-	{#if $currentCoverUrl$}
-		<img class="cover" src={$currentCoverUrl$} alt="cover" on:load={imageLoaded} />
-	{:else}
-		<div class="cover">
-			<svg
-				width="100%"
-				height="100%"
-				viewBox="0 0 512 512"
-				aria-hidden="true"
-				role="img"
-				xmlns="http://www.w3.org/2000/svg"
-				on:load={imageLoaded}
-			>
-				<g transform="translate(256 256)" transform-origin="128 0">
-					<g transform="translate(0,0) scale(1,1)">
-						<path
-							d="M448 80c8.8 0 16 7.2 16 16V415.8l-5-6.5-136-176c-4.5-5.9-11.6-9.3-19-9.3s-14.4 3.4-19 9.3L202 340.7l-30.5-42.7C167 291.7 159.8 288 152 288s-15 3.7-19.5 10.1l-80 112L48 416.3l0-.3V96c0-8.8 7.2-16 16-16H448zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"
-							fill="currentColor"
-							transform="translate(-256 -256)"
-						></path>
+	<button title={changeAudioTitle} disabled={!!$exportCancelController$} on:click={onSetAudioFile}>
+		{#if $currentCoverUrl$}
+			<img class="cover" src={$currentCoverUrl$} alt="cover" on:load={imageLoaded} />
+		{:else}
+			<div class="cover">
+				<svg
+					width="100%"
+					height="100%"
+					viewBox="0 0 512 512"
+					aria-hidden="true"
+					role="img"
+					xmlns="http://www.w3.org/2000/svg"
+					on:load={imageLoaded}
+				>
+					<g transform="translate(256 256)" transform-origin="128 0">
+						<g transform="translate(0,0) scale(1,1)">
+							<path
+								d="M448 80c8.8 0 16 7.2 16 16V415.8l-5-6.5-136-176c-4.5-5.9-11.6-9.3-19-9.3s-14.4 3.4-19 9.3L202 340.7l-30.5-42.7C167 291.7 159.8 288 152 288s-15 3.7-19.5 10.1l-80 112L48 416.3l0-.3V96c0-8.8 7.2-16 16-16H448zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"
+								fill="currentColor"
+								transform="translate(-256 -256)"
+							></path>
+						</g>
 					</g>
-				</g>
-			</svg>
-		</div>
-	{/if}
+				</svg>
+			</div>
+		{/if}
+	</button>
 	<div class="flex flex-1 flex-col">
 		<div class="flex justify-between w-full header-icons">
 			<button title="Export book to text file" on:click={onExportToText}>
